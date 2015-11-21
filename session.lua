30ebb50c2794c03892654dfd55bf3601ce2bdfef
Giovanni La Perna <randag@users.noreply.github.com>
2015-11-21 23:47:23 +0100

    create file

---
+++ session.lua
@@ -0,0 +1,42 @@
+local session = {}
+
+local db = require('byronmccollum/webscript.io-modules/db2.lua')
+local cookie = require('byronmccollum/webscript.io-modules/cookie.lua')
+
+local session_table = {}
+
+session.id = nil
+session.resume = false
+
+local function read()
+	session_table = db.select('sessions', session.id) or {}
+end
+
+local function write()
+	db.insert('sessions', session.id, session_table)
+end
+
+function session.init(request)
+	local cookies = request.headers['Cookie']
+	if cookies then
+		session.id = cookie.get('SESSION_ID', cookies)
+	end
+	if session.id then
+		session.resume = true
+	else
+		session.id = crypto.sha256(request.headers['User-Agent'] .. os.time()).hexdigest()
+	end
+end
+
+function session.set(name, value)
+	read()
+	session_table[name] = value
+	write()
+end
+
+function session.get(name)
+	read()
+	return session_table[name]
+end
+
+return session
