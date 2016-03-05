2992ae0ecd14cba3992c1913e795cffafcd1e4ad
Giovanni La Perna <consfuso@users.noreply.github.com>
2016-03-05 14:52:37 +0100

    Create urlencode.lua

---
+++ urlencode.lua
@@ -0,0 +1,9 @@
+function urlencode(str)
+   if (str) then
+      str = string.gsub (str, "\n", "\r\n")
+      str = string.gsub (str, "([^%w ])",
+         function (c) return string.format ("%%%02X", string.byte(c)) end)
+      str = string.gsub (str, " ", "+")
+   end
+   return str    
+end
